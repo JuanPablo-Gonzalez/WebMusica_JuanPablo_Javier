3a01ca243addc98bf6a7f38ffc03c63adda84407
renedogj <renedogj@gmail.com>
2022-06-15 16:11:43 +0200

    change css barraNavegacion

--- WebMusicaMVC/views/barraNavegacion.php
+++ WebMusicaMVC/views/barraNavegacion.php
@@ -1,15 +1,23 @@
 	<nav>
-		<ul>
-			<li><a href="#"><?php echo '<img src="'.$url.'../imagenes/logo.jpg" alt="perfil">'; ?></a></li>
-			<li><a href="../controllers/foro_controller.php">Comunidad</a></li>
-			<li><a href="#contact">Canciones</a></li>
-			<li><a href="#sobre">Sobre nosotros</a></li>
-			<li style="float:right">
-				<?php 
-				echo '<a class="active" href="'.$url.'../usuarios/'.$_SESSION["tag"].'">
-				'; ?> 
-					<?php echo '<img src="'.$url.'../imagenes/perfil2.jpg" alt="perfil">'; ?> 
-				</a>
+		<div id="logo" class="parte-nav">
+			<h1><?php echo '<a href="'.$url.'../index.php">'; ?>Music-Network</a></h1>
+		</div>
+		<ul id="listado-nav">
+			<li>
+				<?php echo '<a href="'.$url.'../controllers/foro_controller.php">'; ?>Comunidad</a>
+			</li>
+			<li>
+				<?php echo '<a href="'.$url.'../controllers/foro_controller.php#contact">'; ?>Canciones</a>
+			</li>
+			<li>
+				<?php echo '<a href="'.$url.'../controllers/foro_controller.php#sobre">'; ?>Sobre nosotros</a>
 			</li>
 		</ul>
+		<div id="perfil">
+			<?php echo '
+			<a class="active" href="'.$url.'../usuarios/'.$_SESSION["tag"].'">
+				<img src="'.$url.'../imagenes/perfil2.jpg" alt="perfil">
+			</a>
+			'; ?>
+		</div>
 	</nav>
